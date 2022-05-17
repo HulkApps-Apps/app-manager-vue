411231fe5cc996b313b3fac71be5432fe908c586
Divyank <dvmunjapara@gmail.com>
2022-05-17 11:06:14 +0530

    Added styles

--- dev/serve.js
+++ dev/serve.js
@@ -3,7 +3,7 @@ import Dev from './serve.vue';
 // To register individual components where they are used (serve.vue) instead of using the
 // library as a whole, comment/remove this import and it's corresponding "Vue.use" call
 
-import '../src/components/polaris-vue/src/css/polaris-vue.min.css';
+import '../dist/hulkapps-app-manager.css';
 
 import AppManagerVue from '@/entry.esm';
 // Vue.use(AppManagerVue);
--- dev/serve.vue
+++ dev/serve.vue
@@ -5,10 +5,16 @@ import { Banners } from '@/entry.esm';
 import { AppManagerPlan } from '@/entry.esm';
 import { PlanBanners } from '@/entry.esm';
 import { PBadge } from "../src/components/polaris-vue/src/components/PBadge";
+import PCard from "@/components/polaris-vue/src/components/PCard/PCard";
+import PPage from "@/components/polaris-vue/src/components/PPage/PPage";
+import PDisplayText from "@/components/polaris-vue/src/components/PDisplayText/PDisplayText";
 
 export default Vue.extend({
   name: 'ServeDev',
   components: {
+    PDisplayText,
+    PPage,
+    PCard,
     PBadge,
     PlanBanners,
     Banners,
@@ -18,11 +24,21 @@ export default Vue.extend({
 </script>
 
 <template>
-  <div id="app">
-    <PBadge status="success">badge</PBadge>
-    <Banners type="header" />
-    <Banners type="footer" />
-    <AppManagerPlan v-bind:shop_domain="'demo-chirag-parmar.myshopify.com'" />
+  <div>
+    <h1>Test</h1>
+    <div id="app" class="app-manager">
+      <div class="app-manager-body">
+        <PBadge status="success">badge</PBadge>
+        <PPage>
+          <PCard sectioned>
+            <PDisplayText size="large">Test</PDisplayText>
+          </PCard>
+        </PPage>
+      </div>
+      <!--    <Banners type="header" />-->
+      <!--    <Banners type="footer" />-->
+      <!--    <AppManagerPlan v-bind:shop_domain="'demo-chirag-parmar.myshopify.com'" />-->
 
+    </div>
   </div>
 </template>
