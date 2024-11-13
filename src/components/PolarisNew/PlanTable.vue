57d73a01b1ebe2c28ccfc5b5485fc1b74ab85dbe
edis-sc <edis@shopcircle.co>
2024-11-13 16:59:42 +0100

    (changed) further adjusted component for new design

--- src/components/PolarisNew/PlanTable.vue
+++ src/components/PolarisNew/PlanTable.vue
@@ -1,7 +1,7 @@
 <script>
-import VariantButton from "./VariantButton.vue";
 import Swiper, { Navigation, Pagination } from "swiper";
 import "swiper/swiper-bundle.css";
+import VariantButton from "./VariantButton";
 
 export default {
   name: "PlanTable",
@@ -15,9 +15,8 @@ export default {
     },
   },
   methods: {
-    handleClick() {
-      event.preventDefault();
-      this.$emit("click", event);
+    handlePlanClick(plan) {
+      this.$emit("plan-clicked", plan);
     },
     translateMe(message) {
       return this.$translations.hasOwnProperty(message)
@@ -30,18 +29,44 @@ export default {
         : Object.values(plan.features);
       return featuresArray.some((f) => f.feature_id === feature.feature_id);
     },
-    syncHeights() {
-      console.log("Syncing heights");
-      const featureNames = document.querySelectorAll(".table-feature-name");
-      const features = document.querySelectorAll(".table-feature");
-
-      featureNames.forEach((featureName, index) => {
-        const feature = features[index];
-        if (featureName && feature) {
-          // Set the height of the `.feature` element to match the `.feature-name`
-          feature.style.height = `${featureName.offsetHeight}px`;
-        }
+    syncHeights(which = "features") {
+      this.$nextTick(() => {
+        setTimeout(() => {
+          const slides = document.querySelectorAll(".swiper-slide");
+          if (which === "features") {
+            const featureNames =
+              document.querySelectorAll(".plan-feature-name");
+            slides.forEach((slide) => {
+              const features = slide.querySelectorAll(".plan-feature");
+              featureNames.forEach((featureName, index) => {
+                const feature = features[index];
+                if (featureName && feature) {
+                  feature.style.height = `${featureName.offsetHeight}px`;
+                }
+              });
+            });
+          } else if (which === "plans") {
+            const planNames = document.querySelectorAll(".plan-header-wrapper");
+            const plansAvailableName =
+              document.querySelector(".plans-available");
+            let planNameHeight = 0;
+            slides.forEach((slide, index) => {
+              const planName = planNames[index];
+              if (planName) {
+                planNameHeight = Math.max(
+                  planName.offsetHeight,
+                  planNameHeight
+                );
+              }
+              plansAvailableName.style.height = `${planNameHeight}px`;
+            });
+          }
+        }, 0); // delay 0ms
       });
+    },
+    syncAllHeights() {
+      this.syncHeights("features");
+      this.syncHeights("plans");
     },
   },
 
@@ -70,7 +95,7 @@ export default {
   },
 
   mounted() {
-    new Swiper(this.$refs.swiperAnnuallyTable, {
+    const swiperInstance = new Swiper(this.$refs.swiperAnnuallyTable, {
       modules: [Navigation, Pagination],
       loop: false,
       slidesPerView: 3,
@@ -83,14 +108,13 @@ export default {
         nextEl: ".swiper-plan-next",
         prevEl: ".swiper-plan-prev",
       },
-      // After Swiper initialization, synchronize the heights
       on: {
-        slideChange: this.syncHeights, // Sync heights after each slide change
+        slideChange: this.syncAllHeights, // Run syncHeights on each slide change
+        afterInit: this.syncAllHeights, // Sync heights after initial Swiper setup
       },
     });
 
-    // Sync heights once on mount as well
-    this.syncHeights();
+    this.syncAllHeights(); // Run syncHeights once after mount
   },
 };
 </script>
@@ -99,7 +123,7 @@ export default {
   <div class="container">
     <div class="pricing-table">
       <div class="pricing-table-inner__left">
-        <div class="table-header">
+        <div class="table-header plans-available">
           {{ plans.length }} {{ translateMe("Plans available") }}
         </div>
         <div
@@ -116,10 +140,30 @@ export default {
             v-for="(plan, index) in annualPlans"
             :key="plan.id"
             class="swiper-slide"
+            :class="{ 'last-slide': index === annualPlans.length - 1 }"
           >
-            <h1>
-              {{ plan.name }}
-            </h1>
+            <div class="plan-header-wrapper">
+              <div class="upper">
+
+                <h4>{{ plan.name }}</h4>
+                <h4>
+                  ${{ plan.price }}
+                  <h6>
+                    {{
+                      plan.interval === "EVERY_30_DAYS"
+                      ? translateMe("/mo")
+                      : translateMe("/yr")
+                    }}
+                </h6>
+              </h4>
+            </div>
+              <VariantButton
+                :variant="'primary'"
+                @click="handlePlanClick(plan)"
+                class="button"
+                >{{ translateMe("Choose Plan") }}</VariantButton
+              >
+            </div>
             <div
               class="plan-feature"
               v-for="(feature, index) in annualPlansFeatures"
@@ -179,6 +223,22 @@ export default {
   padding: 16px;
   border-bottom: 1px solid #e3e3e3;
 }
+.plan-header-wrapper {
+  padding: 16px;
+  border-bottom: 1px solid #e3e3e3;
+}
+.plan-feature {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-bottom: 1px solid #e3e3e3;
+  border-left: 1px solid #e3e3e3;
+}
+
+.last-slide .plan-feature {
+  border-right: 1px solid #e3e3e3;
+}
+
 .plan-feature-name {
   font-size: 13px;
   font-weight: 450;
@@ -189,5 +249,29 @@ export default {
 .plan-table-checkmark {
   width: 20px;
   height: 20px;
+}
+.plan-header-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 12px;
+  background-color: #f1f1f1;
+  border-left: 1px solid #f1f1f1;
+  border-right: 1px solid #f1f1f1;
+}
+.plan-header-wrapper .upper h4 {
+  display: inline;
+  font-size: 16px;
+  font-weight: 700;
+  color: #303030;
+}
+.plan-header-wrapper .upper h4 h6 {
+  display: inline;
+  font-size: 13px;
+  font-weight: 400;
+  color: #00000080;
+  margin-left: -4px;
+  line-height: 0;
 }
 </style>
